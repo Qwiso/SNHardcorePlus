e86f3e1feaee610d4e09834c12a7454435ae3e07
Stephen England <StephenEngland1986@gmail.com>
2018-05-08 18:57:14 -0500

    Add example config file, sort string conversion to tech type

--- SNHardcorePlus/SubnauticaTechTypeExtensions.cs
+++ SNHardcorePlus/SubnauticaTechTypeExtensions.cs
@@ -8,47 +8,38 @@
             {
                 switch (techTypeAsString.ToLower())
                 {
-                    case "seamoth": return TechType.SeamothFragment;
-                    case "seaglide": return TechType.SeaglideFragment;
-                    case "statisrefile": return TechType.StasisRifleFragment;
-                    case "prawn": return TechType.ExosuitFragment;
-                    case "workbench": return TechType.WorkbenchFragment;
-                    case "propulsioncannon": return TechType.PropulsionCannonFragment;
+                    case "batterycharger": return TechType.BatteryChargerFragment;
+                    case "beacon": return TechType.BeaconFragment;
                     case "bioreactor": return TechType.BioreactorFragment;
-                    case "thermalplant": return TechType.ThermalPlantFragment;
-                    case "nuclearreactor": return TechType.NuclearReactorFragment;
-                    case "moonpool": return TechType.MoonpoolFragment;
-                    case "waterfiltration": return TechType.BaseFiltrationMachineFragment;
-                    case "cyclopshull": return TechType.CyclopsHullFragment;
                     case "cyclopsbridge": return TechType.CyclopsBridgeFragment;
                     case "cyclopsengine": return TechType.CyclopsEngineFragment;
-                    case "powertransmitter": return TechType.PowerTransmitterFragment;
-                    case "modificationbay": return TechType.BaseUpgradeConsoleFragment;
-                    case "observatory": return TechType.BaseObservatoryFragment;
-                    case "batterycharger": return TechType.BatteryChargerFragment;
-                    case "powercellcharger": return TechType.PowerCellChargerFragment;
-                    case "scannerroom": return TechType.ScannerRoomFragment;
-                    case "specimenanalyzer": return TechType.SpecimenAnalyzerFragment;
-                    case "growbed": return TechType.FarmingTrayFragment;
-                    case "sign": return TechType.SignFragment;
-                    case "pictureframe": return TechType.PictureFrameFragment;
-                    case "bench": return TechType.BenchFragment;
-                    case "planterpot": return TechType.PlanterPotFragment;
-                    case "planterbox": return TechType.PlanterBoxFragment;
-                    case "plantershelf": return TechType.PlanterShelfFragment;
-                    case "aquarium": return TechType.AquariumFragment;
-                    case "reinforceddivesuit": return TechType.ReinforcedDiveSuitFragment;
-                    case "stillsuit": return TechType.StillsuitFragment;
-                    case "ledlight": return TechType.LEDLightFragment;
-                    case "techlight": return TechType.TechlightFragment;
-                    case "spotlight": return TechType.SpotlightFragment;
-                    case "lasercutter": return TechType.LaserCutterFragment;
-                    case "beacon": return TechType.BeaconFragment;
+                    case "cyclopshull": return TechType.CyclopsHullFragment;
+                    case "floodlight": return TechType.TechlightFragment;
                     case "gravsphere": return TechType.GravSphereFragment;
-                    case "drillarm": return TechType.ExosuitDrillArmFragment;
-                    case "propulsionarm": return TechType.ExosuitPropulsionArmFragment;
-                    case "grapplingarm": return TechType.ExosuitGrapplingArmFragment;
+                    case "gravtrap": return TechType.GravSphereFragment;
+                    case "lasercutter": return TechType.LaserCutterFragment;
+                    case "modificationbay": return TechType.BaseUpgradeConsoleFragment;
+                    case "moonpool": return TechType.MoonpoolFragment;
+                    case "nuclearreactor": return TechType.NuclearReactorFragment;
+                    case "powercellcharger": return TechType.PowerCellChargerFragment;
+                    case "powertransmitter": return TechType.PowerTransmitterFragment;
+                    case "prawn": return TechType.ExosuitFragment;
+                    case "prawndrillarm": return TechType.ExosuitDrillArmFragment;
+                    case "prawngrapplingarm": return TechType.ExosuitGrapplingArmFragment;
+                    case "prawnpropulsionarm": return TechType.ExosuitPropulsionArmFragment;
+                    case "prawnpropulsioncannon": return TechType.ExosuitPropulsionArmFragment;
+                    case "prawntorpoedoarm": return TechType.ExosuitTorpedoArmFragment;
+                    case "propulsioncannon": return TechType.PropulsionCannonFragment;
+                    case "reinforceddivesuit": return TechType.ReinforcedDiveSuitFragment;
+                    case "scannerroom": return TechType.ScannerRoomFragment;
+                    case "seaglide": return TechType.SeaglideFragment;
+                    case "seamoth": return TechType.SeamothFragment;
+                    case "statisrefile": return TechType.StasisRifleFragment;
+                    case "stillsuit": return TechType.StillsuitFragment;
+                    case "techlight": return TechType.TechlightFragment;
+                    case "thermalplant": return TechType.ThermalPlantFragment;
                     case "torpedoarm": return TechType.ExosuitTorpedoArmFragment;
+                    case "waterfiltration": return TechType.BaseFiltrationMachineFragment;
                 }
             }
 
